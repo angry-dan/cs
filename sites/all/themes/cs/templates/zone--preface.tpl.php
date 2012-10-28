8997d675d385a544a2195148cf936b87911a8dbc
Dan James <danj@deeson.co.uk>
2012-10-28 12:11:56 +0000

    Fixing breadcrumb spacing

--- sites/all/themes/cs/templates/zone--preface.tpl.php
+++ sites/all/themes/cs/templates/zone--preface.tpl.php
@@ -1,10 +1,10 @@
-<?php if ($wrapper): ?><div<?php print $attributes; ?>><?php endif; ?>  
+<?php if ($wrapper): ?><div<?php print $attributes; ?>><?php endif; ?>
   <div<?php print $content_attributes; ?>>
     <?php if ($breadcrumb): ?>
-      <div id="breadcrumb" class="grid-4 push-1"><?php print $breadcrumb; ?></div>
+      <div id="breadcrumb" class="grid-4 push-1 suffix-4"><?php print $breadcrumb; ?></div>
     <?php else: ?>
       <div class="grid-8 push-1">&nbsp;</div>
-    <?php endif; ?>    
+    <?php endif; ?>
     <?php print $content; ?>
   </div>
 <?php if ($wrapper): ?></div><?php endif; ?>
