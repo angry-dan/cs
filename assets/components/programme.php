c47ee59c52ac7bb26741df58330f91ea651792e0
Dan James <danj7c3@gmail.com>
2023-08-04 14:56:04 +0100

    adding events calendar

--- assets/components/programme.php
+++ assets/components/programme.php
@@ -11,33 +11,38 @@
 </head>
 <body>
 <header>
-  <?php print theme('navigation', ['content' => ['current' => 'home']]); ?>
+  <?php print theme("navigation", ["content" => ["current" => "home"]]); ?>
 </header>
 
 <main class="container">
   <div class="text">
     <h1>Programme</h1>
-    <?php print theme('programme', [
-      'content' => [
+    <?php print theme("programme", [
+      "content" => [
         [
-          'id' => "beavers",
-          'label' => "Beavers",
-          'calendar' => 'concordescouts.org.uk_5a5pr9kplmld8g2e54sklhk3l8%40group.calendar.google.com',
+          "id" => "events",
+          "label" => "Events",
+          "calendar" => "c_296a94e1f9adf9a0988eff2eadbd1ce63a73fd2910760d7fe366b4f8c3c4377f@group.calendar.google.com",
         ],
         [
-          'id' => "cubs",
-          'label' => "Cubs",
-          'calendar' => 'concordescouts.org.uk_gubd38jbitgsm8cokfe539gtrk%40group.calendar.google.com',
+          "id" => "beavers",
+          "label" => "Beavers",
+          "calendar" => "concordescouts.org.uk_5a5pr9kplmld8g2e54sklhk3l8%40group.calendar.google.com",
         ],
         [
-          'id' => "scouts",
-          'label' => "Scouts",
-          'calendar' => 'concordescouts.org.uk_smugdfso66dvnr4alq4ft2lvbs%40group.calendar.google.com',
+          "id" => "cubs",
+          "label" => "Cubs",
+          "calendar" => "concordescouts.org.uk_gubd38jbitgsm8cokfe539gtrk%40group.calendar.google.com",
         ],
         [
-          'id' => "explorers",
-          'label' => "Explorers",
-          'calendar' => 'concordescouts.org.uk_vrm13n3jsrhqsek89pc3apuhgc%40group.calendar.google.com',
+          "id" => "scouts",
+          "label" => "Scouts",
+          "calendar" => "concordescouts.org.uk_smugdfso66dvnr4alq4ft2lvbs%40group.calendar.google.com",
+        ],
+        [
+          "id" => "explorers",
+          "label" => "Explorers",
+          "calendar" => "concordescouts.org.uk_vrm13n3jsrhqsek89pc3apuhgc%40group.calendar.google.com",
         ],
       ],
     ]); ?>
